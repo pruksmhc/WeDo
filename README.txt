3571ede950cafac5c02462d1ee4be17fa17b8ae2
Yada Pruksachatkun <pruks22y@mtholyoke.edu>
2015-11-03 09:15:22 -0500

    Title: Creating more front-end:

--- README.txt
+++ README.txt
@@ -3,3 +3,12 @@ How to run:
 -Start the rethinkdb server by calling "rethinkdb", then open another command line tab and then run node server.js
 https://www.rethinkdb.com/docs/start-a-server/
 -Go to http://www.wedoappbeta.com , and if you have access, you'll be able to log in and start using it. 
+
+Status of Progress:
+Current features working:
+-Tested API for creating to-do items 
+-Facebook login 
+
+Features to work on (most pertinent):
+-Front-end UI for creating projects, viewing personal projects in own dashboar d
+-Privacy settings to be able to merge to-do lists. 
--- templates/js/controllers.js
+++ templates/js/controllers.js
@@ -6,3 +6,31 @@ wedoApp.controller('HomeCtrlr',  ['$scope', function ($scope) {
   $scope.fullName = "YADA"; 
 }]);
 //coontains the contorlelrs for all the html files. 
+
+wedoApp.controller("CreateProjectController", function($scope, $http){
+	  $scope.duedate = ""; 
+    $scope.todoTitle = ""; 
+    $scope.tododescription = ""; 
+
+	$scope.generateTodoItem = function(){
+	console.log("WOAH"); 
+	console.log("Scope was "+ $scope.duedate+$scope.todoTitle+$scope.tododescription); 
+//	console.log("The things gotten are " + $scope.duedate  +  $scope.todoTitle + $scope.tododescription); 
+	$http({
+      		method: "POST", 
+      		url: "http://yadas-air:3000/todoitem", 
+      		data: {
+      				title:  $scope.todoTitle  , 
+   		description: $scope.tododescription ,
+   		due_date: $scope.duedate
+      		}
+    	}).success(function(data) {
+    		console.log("Success! Data was passed in and this happened"+JSON.stringify(data));
+    		
+    	}).error(function(err) {
+    		console.log("Error was "+JSON.stringify(err)); 
+    		
+    	});
+    };
+
+});
